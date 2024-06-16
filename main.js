361a6a398c1bf51ecce561409511a0462443732c
DU <sangdyjjang@naver.com>
2024-06-16 13:37:18 +0900

    vault backup: 2024-06-16 13:37:18

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
+function templateHTML(title, list, body) {
+  return `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${body}
+          </body>
+          </html>
+          `;
+}
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -19,21 +36,11 @@ var app = http.createServer(function (request, response) {
           i = i + 1;
         }
         list = list + "</ul>";
-        var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2><p>${description}</p>`
+        );
         response.writeHead(200);
         response.end(template);
       });
