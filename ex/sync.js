567bed1360f2f8c3c6dfcbee18be13195cdf6045
DU <sangdyjjang@naver.com>
2024-06-16 13:58:12 +0900

    vault backup: 2024-06-16 13:58:12

--- ex/sync.js
+++ ex/sync.js
@@ -1,1 +1,12 @@
 var fs = require("fs"); // require() 함수를 이용하여 fs 모듈을 불러온다. fs 모듈은 파일 시스템에 접근하는 모듈이다.
+//ReadFileSync
+
+// console.log("A");
+// var result = fs.readFileSync("./ex/sample.txt", "utf8");
+// console.log(result);
+// console.log("C");
+
+console.log("A");
+var result = fs.readFile("./ex/sample.txt", "utf8");
+console.log(result);
+console.log("C");
